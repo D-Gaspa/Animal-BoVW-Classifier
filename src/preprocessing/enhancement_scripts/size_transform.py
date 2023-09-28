27db7364b516b1df593fc1b3c67dd0a2d87fd478
diego <diego.gaspariseo@udlap.mx>
2023-09-28 13:14:16 -0600

    Size transform change

--- src/preprocessing/enhancement_scripts/size_transform.py
+++ src/preprocessing/enhancement_scripts/size_transform.py
@@ -3,13 +3,27 @@ from PIL import Image
 from PIL.Image import Resampling
 
 
-def _resize_image(image_path, output_path, new_width):
+def _resize_image(image_path, output_path, min_size):
     try:
+        # change the image size depending on the minimum size keeping the aspect ratio
         image = Image.open(image_path)
-        w_percent = (new_width / float(image.size[0]))
-        new_height = int((float(image.size[1]) * float(w_percent)))
-        resized_image = image.resize((new_width, new_height), Resampling.LANCZOS)
+        width, height = image.size
 
+        # check if the image is already bigger than the minimum size
+        if width > min_size and height > min_size:
+            return
+
+        if width < height:
+            new_width = min_size
+            new_height = int(height * (min_size / width))
+        else:
+            new_height = min_size
+            new_width = int(width * (min_size / height))
+
+        # resize the image
+        resized_image = image.resize((new_width, new_height), resample=Resampling.BICUBIC)
+
+        # save the resized image
         save_path = os.path.join(output_path, os.path.basename(image_path))
         print(f"Saving to: {save_path}")
         resized_image.save(save_path)
