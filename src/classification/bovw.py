676dfb8bb0fa7471657f7c590a1e19bd9d4a98b1
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2023-10-04 14:24:50 -0600

    Implemented image loading and classification preparation using ORB

---
+++ src/classification/bovw.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2 as cv
+import os
+import pprint
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+
+
+data_path = f'{Path.cwd()}\\data\\enhanced_images'
+categories = []
+data = []
+labels = []
+descriptors = []
+
+
+def prep_data():
+    global labels
+    for categories_index, class_name in enumerate(os.listdir(data_path)):
+        categories.append(class_name)
+        image_path = os.path.join(data_path, class_name)
+        for image_file in os.listdir(image_path):
+            image = cv.imread(os.path.join(image_path, image_file))
+            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+            image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+            data.append(image.flatten())
+            labels.append(categories_index)
+prep_data()         
+data = np.asarray(data)  
+#labels = np.asarray(labels)
+"""
+X_train, X_test, Y_train, Y_test = train_test_split(data, labels, test_size= 0.3, train_size= 0.7, shuffle= True, random_state = 35, stratify=labels)
+
+orb = cv.ORB.create()
+
+for img in X_train:
+    kp, des = orb.detectAndCompute(X_test, None)
+
+for d in des:
+    descriptors.append(d)
+"""
+print(data)
+
+#print(des)
