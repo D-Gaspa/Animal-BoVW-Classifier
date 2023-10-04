18a266a8b6b3535da64d48ecd3947f58557dde7f
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2023-10-04 14:24:50 -0600

    Preparing dataset for classification

---
+++ src/classification/bovw.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2 as cv
+import os
+import pprint
+from pathlib import Path
+
+
+data_path = f'{Path.cwd()}\\data\\enhanced_images'
+labels = []
+data = []
+
+
+def prep_data():
+    for class_name in os.listdir(data_path):
+        labels.append(class_name)
+        image_path = os.path.join(data_path, class_name)
+        for image_file in os.listdir(image_path):
+            image = cv.imread(os.path.join(image_path, image_file))
+            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+            image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+            data.append(image)
+        
+
+prep_data()
+
+
